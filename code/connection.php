6fa232fe0223190a96a02beae1deeb183f6b6b96
Omair Aljabri <Oaksaadallah@gmail.com>
2022-05-14 21:24:15 +0300

    Add files via upload

---
+++ code/connection.php
@@ -0,0 +1,14 @@
+
+<?php
+
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "project";
+	$dbscore=0;
+
+	if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbscore))
+	{
+
+		die("failed to connect!");
+	}
